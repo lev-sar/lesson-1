b015733d2a8ce5f9f22520b3c4473ba8f2f45a6a
lev-sar <lev.sr5@yandex.ru>
2021-12-14 11:40:42 +0300

    Задания к 1 уроку

---
+++ dz1.py
@@ -0,0 +1,13 @@
+# Задание 1
+
+# duration = int(input('Введите значение'))
+# day = duration // 86400
+# hour = duration // 3600
+# minute = duration // 60 % 60
+# second = duration % 60
+# print('День:',day,'Час:',hour,'минут:',minute,'Сек:', second)
+#
+
+
+
+
